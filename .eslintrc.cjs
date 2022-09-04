ff7f9855281e9b07b81001dc99cf43f76d294786
99-zziy <wldud8229@gmail.com>
2022-09-05 01:09:24 +0900

    :sparkles: layout - style

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react/jsx-key': 'warn',
+    'react/no-unescaped-entities': 'warn',
+  },
+}
